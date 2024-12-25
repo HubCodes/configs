017ca1493fb9694a92b2284e648d09c9aa7e361a
HubCodes <krlrhkstk12@gmail.com>
2024-12-25 18:41:18 +0900

    home-manager

---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "taemin";
+  home.homeDirectory = "/home/taemin";
+
+  home.packages = with pkgs; [
+    neofetch
+    zip
+    xz
+    unzip
+
+    jq
+    fzf
+
+    dnsutils
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+
+    nix-output-monitor
+
+    strace
+    ltrace
+    lsof
+
+    lm_sensors # sensors
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "HubCodes";
+    userEmail = "krlrhkstk12@gmail.com";
+  };
+
+   programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+    '';
+
+    shellAliases = {
+      g = "git";
+    };
+  };
+
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
